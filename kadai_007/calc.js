542f036d8205ef257c81cc526aa5009a69f447a3
yuki1017prin <126440804+yuki1017prin@users.noreply.github.com>
2025-05-11 14:53:21 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+let num = 71;
+
+
+if(num%3===0 && num%5===0){
+    console.log("３と５の倍数です");
+}
+
+else if(num%3===0){
+    console.log("３の倍数です");
+}
+
+else if(num%5===0){
+    console.log("５の倍数です");
+}
+
+else{
+    console.log(num);
+}
