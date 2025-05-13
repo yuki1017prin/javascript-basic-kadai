61b4d313483c4fb40eecf12d975e923174d597d7
yuki1017prin <126440804+yuki1017prin@users.noreply.github.com>
2025-05-13 13:58:46 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,8 +0,0 @@
-function getTodayFormatted() {
-  const today = new Date();
-  const formattedDate = today.toISOString().split('T')[0];
-  console.log(formattedDate);
-}
-
-// 関数呼び出し
-getTodayFormatted(); // 出力例: 2025-05-13
