e5c1431ede6160f95d96f45c0b75366efe0fd319
yuki1017prin <126440804+yuki1017prin@users.noreply.github.com>
2025-05-21 17:32:33 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,5 @@
+const textChange=document.getElementById("btn");
+textChange.addEventListener("click",()=>{
+    const text=document.getElementById("text");
+    text.textContent="ボタンをクリックしました";
+});
